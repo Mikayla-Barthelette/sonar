5130b0f5b55ff128ff6060aa5f461f4151a99eab
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-02 12:46:34 -0400

    added sonar

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+// Created by: Mikayla Barthelette
+// Created on: Oct. 2020
+// 
+// This program uses a sonar to find distance
+let distance = 0
+basic.showIcon(IconNames.Yes)
+basic.pause(500)
 basic.forever(function () {
-	
+    distance = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
 })
